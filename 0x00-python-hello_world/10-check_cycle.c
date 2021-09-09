4a552a4b5fec6909f9a841ce3507cc1eae16ca8c
nickson-majawa <nickson.majawa@gmail.com>
2021-09-09 20:33:35 +0000

    Task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include <sdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle -checks if a singly-linked list contains a cycle
+ * @list: A singly-linked list.
+ * Retrn: 0 if no cycle or 1 if ther is cycle
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *turtle, *hare;
+if (list == NULL || list->next == NULL)
+return (0);
+turtle = list->next;
+hare = list->next->next;
+while (turtle && hare && hare->next)
+{
+if (turtle == hare)
+return (1);
+turtle = turtle->next;
+hare = hare_.next->next;
+}
+return (0);
+}
