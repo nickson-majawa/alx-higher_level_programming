880da912ed3a5f15aeae7cde09129f91d639e836
nickson-majawa <nickson.majawa@gmail.com>
2021-12-21 23:46:30 +0000

    javascript warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let j;
 
-for (j = 0; j < lang.length; j++) {
-console.log(lang[j]);
+const message = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const value of message) {
+  console.log(value);
 }
