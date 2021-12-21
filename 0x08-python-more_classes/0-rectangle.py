a0c2777ba03aa7f6663c7aa7e38db8787d87648f
nickson-majawa <nickson.majawa@gmail.com>
2021-12-21 21:58:50 +0000

    Update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 0-rectangle
+
+    Empty Rectangle class
+"""
+
+class Rectangle():
+    """Rectangle Class
+    Empty class
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+# 0x08 Python Project
+More Classes and Objects
