53d3a5996fd506b9734a76e633e092843f4ac1e6
nickson-majawa <nickson.majawa@gmail.com>
2022-01-02 23:57:32 +0000

    update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,17 +8,20 @@
  */
 int check_cycle(listint_t *list)
 {
-listint_t *turtle, *hare;
-if (list == NULL || list->next == NULL)
-return (0);
-turtle = list->next;
-hare = list->next->next;
-while (turtle && hare && hare->next)
-{
-if (turtle == hare)
-return (1);
-turtle = turtle->next;
-hare = hare_.next->next;
-}
-return (0);
+   listint_t *turtle, *hare;
+   if (list == NULL || list->next == NULL)
+     return (0);
+
+   turtle = list->next;
+   hare = list->next->next;
+
+   while (turtle && hare && hare->next)
+     {
+       if (turtle == hare)
+	 return (1);
+
+       turtle = turtle->next;
+       hare = hare_.next->next;
+     }
+   return (0);
 }
