79dfa36ae12cdf585dd968ae70c4f1ca2b9d1e89
nickson-majawa <nickson.majawa@gmail.com>
2022-01-08 19:44:07 +0000

    Task 0x10

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -H "X-School-User-Id: 98"
+curl -s "$1" -X GET -S "X-School-User-Id: 98"
 
