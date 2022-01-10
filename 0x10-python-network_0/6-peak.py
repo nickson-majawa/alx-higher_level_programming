4dd58ad359c5976dc8d552409e4d0b9e2ed69bef
nickson-majawa <nickson.majawa@gmail.com>
2022-01-10 12:42:59 +0000

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,7 +12,7 @@ def find_peak(list_of_integers):
         Max integer or None
     """
     if list_of_integers:
-        list_of_integers.sort()
-        return list_of_integers[-1]
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
     else:
         return None
