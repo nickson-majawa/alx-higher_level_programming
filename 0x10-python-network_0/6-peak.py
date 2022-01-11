4e18a7ce717d9392bffedee9ed8ef171ede0f1a8
nickson-majawa <nickson.majawa@gmail.com>
2022-01-11 22:40:06 +0000

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,5 +13,5 @@ def find_peak(list_of_integers):
     """
     if list_of_integers:
         list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
-        
+        temp = list_of_integers[0]
+        return temp
